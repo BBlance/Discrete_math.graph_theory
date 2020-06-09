b662c43d170a2b9f2733cc83560685b46f250a24
lixiaolong1996 <957175186@163.com>
2020-06-10 02:44:47 +0800

    常规更新

---
+++ src/ui_DataDetails.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'DataDetails.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
+    QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter,
+    QPixmap, QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_DataDetails(object):
+    def setupUi(self, DataDetails):
+        if not DataDetails.objectName():
+            DataDetails.setObjectName(u"DataDetails")
+        DataDetails.resize(400, 300)
+        self.verticalLayout_2 = QVBoxLayout(DataDetails)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        self.dataTable = QTableView(DataDetails)
+        self.dataTable.setObjectName(u"dataTable")
+
+        self.verticalLayout_2.addWidget(self.dataTable)
+
+
+        self.retranslateUi(DataDetails)
+
+        QMetaObject.connectSlotsByName(DataDetails)
+    # setupUi
+
+    def retranslateUi(self, DataDetails):
+        DataDetails.setWindowTitle(QCoreApplication.translate("DataDetails", u"Form", None))
+    # retranslateUi
+
---
+++ src/ui_ShowMatrix.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ShowMatrix.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
+    QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter,
+    QPixmap, QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_ShowMatrix(object):
+    def setupUi(self, ShowMatrix):
+        if not ShowMatrix.objectName():
+            ShowMatrix.setObjectName(u"ShowMatrix")
+        ShowMatrix.resize(274, 210)
+        self.verticalLayout = QVBoxLayout(ShowMatrix)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.layout = QVBoxLayout()
+        self.layout.setObjectName(u"layout")
+
+        self.verticalLayout.addLayout(self.layout)
+
+        self.dataView = QTableView(ShowMatrix)
+        self.dataView.setObjectName(u"dataView")
+
+        self.verticalLayout.addWidget(self.dataView)
+
+
+        self.retranslateUi(ShowMatrix)
+
+        QMetaObject.connectSlotsByName(ShowMatrix)
+    # setupUi
+
+    def retranslateUi(self, ShowMatrix):
+        ShowMatrix.setWindowTitle(QCoreApplication.translate("ShowMatrix", u"Form", None))
+    # retranslateUi
+
